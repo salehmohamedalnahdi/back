2e01d1e5ff68c389fa3365fc6d45b3ce2f6e4371
salehmohamedalnahdi <smbt2011@gmail.com>
2024-05-26 12:58:45 +0300

    t

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const io = require('socket.io')(http);
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
