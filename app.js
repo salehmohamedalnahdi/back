e93fac277d9c94062251b590fb7a87905654569c
salehmohamedalnahdi <smbt2011@gmail.com>
2024-05-27 19:01:55 +0300

    ff

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 // server.js
 
+// app.js
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Store connected users
+const users = {};
+
+// Socket.IO event handlers
+io.on('connection', (socket) => {
+  console.log('New socket connection:', socket.id);
+
+  // Handle new user joining the chat
+  socket.on('join', (username) => {
+    users[socket.id] = username;
+    io.emit('userJoined', username);
+  });
+
+  // Handle incoming messages
+  socket.on('message', (message) => {
+    io.emit('message', { username: users[socket.id], message });
+  });
+
+  // Handle user leaving the chat
+  socket.on('disconnect', () => {
+    const username = users[socket.id];
+    delete users[socket.id];
+    io.emit('userLeft', username);
+  });
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+
+
+/*const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -9,7 +57,7 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://back-l9k2.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
@@ -28,10 +76,10 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = 8000;
+const port = 3000;
 http.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+});*/
 
 
 /*const express= require('express')
