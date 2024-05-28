fac69123401b4fd4155ed1a0d9793a2b2bc4919b
salehmohamedalnahdi <smbt2011@gmail.com>
2024-05-28 17:58:06 +0300

    hfks

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 // server.js
 
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const cors=require("cors")
+const {Server} = require('socket.io');
+
+app.use(cors())
+const io =new Server(http,{
+  cors: {
+    origin:"http://localhost:3000",
+    methods: ["GET",'POST']
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (data) => {
+    console.log('Received message:', data);
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const port = 4000;
+http.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+/*
 // app.js
 const express = require('express');
 const http = require('http');
@@ -58,7 +104,7 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('Server started on port 3000');
 });
-
+*/
 
 
 /*const express = require('express');
